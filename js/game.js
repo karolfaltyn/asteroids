9b60df0abc68e9b6778ebc38c1fa0d74e67fd64f
Karol Faltyn <karolfaltyn03@gmail.com>
2023-08-11 13:33:52 +0200

    added ship movement

--- js/game.js
+++ js/game.js
@@ -17,13 +17,31 @@ Game = {
         Game.ctx = Game.canvas.getContext('2d');
         Game.layout();
 
-        window.addEventListener('resize', Game.layout, false);
+        window.addEventListener('resize', Game.layout);
 
         document.body.appendChild(Game.canvas);
 
         Game.ship = new Ship();
 
+        window.addEventListener('keydown', Game.onKey);
+        window.addEventListener('keyup', Game.onKey);
+
         Game.animationLoop();
+    },
+
+    onKey: function (ev) {
+        if (ev.keyCode == 32 || ev.keyCode == 37 || ev.keyCode == 38 || ev.keyCode == 39) {
+            if (ev.type == 'keydown' && !Game['key_' + ev.keyCode]) {
+                Game['key_' + ev.keyCode] = true;
+                if (ev.keyCode == 37) {
+                    Game.key_39 = false;
+                } else if (ev.keyCode == 39) {
+                    Game.key_37 = false;
+                }
+            } else if (ev.type == 'keyup') {
+                Game['key_' + ev.keyCode] = false;
+            }
+        }
     },
     layout: function (ev) {
         VAR.W = window.innerWidth;
